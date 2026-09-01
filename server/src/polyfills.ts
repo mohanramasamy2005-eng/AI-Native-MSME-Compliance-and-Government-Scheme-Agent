73eeff6cf4787704774ae54ca8556bcc46bfd3e4
mohanramasamy2005-eng <mohanramasamy2005@gmail.com>
2026-09-01 16:51:59 +0530

    fix: add DOMMatrix polyfills for Render deployment

---
+++ server/src/polyfills.ts
@@ -0,0 +1,11 @@
+if (typeof global !== "undefined") {
+  if (typeof (global as any).DOMMatrix === "undefined") {
+    (global as any).DOMMatrix = class DOMMatrix {};
+  }
+  if (typeof (global as any).ImageData === "undefined") {
+    (global as any).ImageData = class ImageData {};
+  }
+  if (typeof (global as any).Path2D === "undefined") {
+    (global as any).Path2D = class Path2D {};
+  }
+}
